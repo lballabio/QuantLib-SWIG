00e68d43345742aa9f7702d147a883185f130308
Wojciech Ślusarski <wojciech.slusarski@gmail.com>
2018-07-18 22:03:05 +0200

    Exteding the test suite
    Signed-off-by: Wojciech Ślusarski <wojciech.slusarski@gmail.com>

--- Python/test/QuantLibTestSuite.py
+++ Python/test/QuantLibTestSuite.py
@@ -33,6 +33,7 @@ from assetswap import AssetSwapTest
 from capfloor import CapFloorTest
 from blackformula import BlackFormulaTest
 from blackformula import BlackDeltaCalculatorTest
+from iborindex import IborIndexTest
 
 
 def test():
@@ -56,6 +57,7 @@ def test():
     suite.addTest(unittest.makeSuite(CapFloorTest, 'test'))
     suite.addTest(unittest.makeSuite(BlackFormulaTest, 'test'))
     suite.addTest(unittest.makeSuite(BlackDeltaCalculatorTest, 'test'))
+    suite.addTest(unittest.makeSuite(IborIndexTest, 'test'))
 
     result = unittest.TextTestRunner(verbosity=2).run(suite)
 
