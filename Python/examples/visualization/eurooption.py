b07febd11030a484b5ceff56f651487ecfce54f6
Luigi Ballabio <luigi.ballabio@gmail.com>
2019-04-30 10:53:35 +0200

    Avoid 'from QuantLib import *'.

--- Python/examples/visualization/eurooption.py
+++ Python/examples/visualization/eurooption.py
@@ -1,40 +1,41 @@
 # Example of option baskets
 # Distributed under BSD license
 
-from QuantLib import *
-todaysDate = Date(15, May, 1998)
-Settings.instance().evaluationDate = todaysDate
-settlementDate = Date(17, May, 1998)
-riskFreeQuote = SimpleQuote(0.05)
-riskFreeRate = FlatForward(settlementDate,
-                           QuoteHandle(riskFreeQuote), Actual365Fixed())
+import QuantLib as ql
+
+todaysDate = ql.Date(15, ql.May, 1998)
+ql.Settings.instance().evaluationDate = todaysDate
+settlementDate = ql.Date(17, ql.May, 1998)
+riskFreeQuote = ql.SimpleQuote(0.05)
+riskFreeRate = ql.FlatForward(settlementDate, ql.QuoteHandle(riskFreeQuote), ql.Actual365Fixed())
 
 # option parameters
-exercise1 = AmericanExercise(settlementDate, Date(17,May,1999))
-exercise2 = EuropeanExercise(settlementDate)
-payoff = PlainVanillaPayoff(Option.Call, 40.0)
+exercise1 = ql.AmericanExercise(settlementDate, ql.Date(17, ql.May, 1999))
+exercise2 = ql.EuropeanExercise(settlementDate)
+payoff = ql.PlainVanillaPayoff(ql.Option.Call, 40.0)
 
 # market data
-underlying = SimpleQuote(36.0)
-volatilityQuote = SimpleQuote(0.05)
-volatility = BlackConstantVol(todaysDate,
-                              QuoteHandle(volatilityQuote),
-                              Actual365Fixed())
-dividendYield = FlatForward(settlementDate, 0.00, Actual365Fixed())
+underlying = ql.SimpleQuote(36.0)
+volatilityQuote = ql.SimpleQuote(0.05)
+volatility = ql.BlackConstantVol(todaysDate, ql.QuoteHandle(volatilityQuote), ql.Actual365Fixed())
+dividendYield = ql.FlatForward(settlementDate, 0.00, ql.Actual365Fixed())
 
 # good to go
-process = BlackScholesMertonProcess(QuoteHandle(underlying),
-                                    YieldTermStructureHandle(dividendYield),
-                                    YieldTermStructureHandle(riskFreeRate),
-                                    BlackVolTermStructureHandle(volatility))
-option1 = VanillaOption(process, payoff, exercise1)
-option1.setPricingEngine(BaroneAdesiWhaleyEngine())
+process = ql.BlackScholesMertonProcess(
+    ql.QuoteHandle(underlying),
+    ql.YieldTermStructureHandle(dividendYield),
+    ql.YieldTermStructureHandle(riskFreeRate),
+    ql.BlackVolTermStructureHandle(volatility),
+)
+option1 = ql.VanillaOption(process, payoff, exercise1)
+option1.setPricingEngine(ql.BaroneAdesiWhaleyEngine())
 
 
-def f(x,y):
+def f(x, y):
     underlying.setValue(x)
     volatilityQuote.setValue(y)
     return option1.NPV()
+
 
 def setQuote(r):
     riskFreeQuote.setValue(r)
