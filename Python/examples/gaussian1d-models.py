906847b81e3bf99d4a27f5e021876b6554656759
Peter Caspers <peter.caspers@quaternion.com>
2019-06-07 18:53:26 +0200

    improve description (thanks to Giuseppe Trapani)

--- Python/examples/gaussian1d-models.py
+++ Python/examples/gaussian1d-models.py
@@ -156,12 +156,12 @@ print("Bermudan swaption NPV (ATM calibrated GSR) = %f" % npv)
 
 # Recalibration to 4$ Strike swaption
 print( "\nThere is another mode to generate a calibration basket called"
-       "\nMaturityStrikeByDeltaGamma. This means that the maturity, "
-       "\nthe strike and the nominal of the calibrating swaption are "
-       "\ncomputed such that the npv and its first and second "
-       "\nderivative with respect to the model's state variable) of"
-       "\nthe exotics underlying match with the calibrating swaption's"
-       "\nunderlying. Let's try this in our case." )
+       "\nMaturityStrikeByDeltaGamma. This means that the maturity,"
+       "\nthe strike and the nominal of the calibrating swaptions are"
+       "\nobtained matching the NPV, first derivative and second derivative"
+       "\nof the swap you will exercise into at at each bermudan call date."
+       "\nThe derivatives are taken with respect to the model's state variable."
+       "\nLet's try this in our case." )
 
 basket = swaption.calibrationBasket(swapBase, swaptionVol, 'MaturityStrikeByDeltaGamma')
 printBasket(basket)
