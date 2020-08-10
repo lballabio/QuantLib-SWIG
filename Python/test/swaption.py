6325e47f2b112f91de1d3b70aa43329bffc08944
Marcin <mrybacki12@gmail.com>
2020-08-10 14:59:14 +0200

    Swaption unit tests for Python - clean up.

--- Python/test/swaption.py
+++ Python/test/swaption.py
@@ -19,6 +19,10 @@ import unittest
 import QuantLib as ql
 
 
+BASIS_POINT = 1e-4
+
+EPSILON = 1.e-10
+
 OPTION_TYPE_MAP = {ql.VanillaSwap.Receiver: 'Receiver',
                    ql.VanillaSwap.Payer: 'Payer'}
 
@@ -31,6 +35,35 @@ SETTLEMENT_METHOD_MAP = {ql.Settlement.PhysicalOTC: 'Physical OTC',
                          ql.Settlement.CollateralizedCashPrice: (
                              'Collateralized Cash Price'),
                          ql.Settlement.ParYieldCurve: 'Par Yield Curve'}
+
+
+def compounded_annual_constant_rate_discount(
+        rate: float,
+        day_counter: ql.DayCounter):
+    def _calc(start: ql.Date, end: ql.Date):
+        time = day_counter.yearFraction(start, end)
+        return (1.0 + rate) ** (-time)
+    return _calc
+
+
+def par_yield_bps(underlying: ql.VanillaSwap,
+                  discount_handle: ql.YieldTermStructureHandle):
+    fixed_leg = underlying.fixedLeg()
+    first_coupon = ql.as_fixed_rate_coupon(fixed_leg[0])
+    discount_date = first_coupon.accrualStartDate()
+    discount = discount_handle.discount(discount_date)
+    fixed_rate = underlying.fixedRate()
+    fixed_dct = underlying.fixedDayCount()
+    fair_rate = underlying.fairRate()
+    ir_func = compounded_annual_constant_rate_discount(fair_rate, fixed_dct)
+    bps = sum([ir_func(discount_date, c_f.date()) * c_f.amount() / fixed_rate
+               for c_f in fixed_leg
+               if c_f.date() > discount_date])
+    return abs(bps) * discount
+
+
+def swap_pv01(underlying: ql.VanillaSwap):
+    return abs(underlying.fixedLegBPS()) / BASIS_POINT
 
 
 def make_const_black_vol_engine(discount_handle, volatility):
@@ -74,164 +107,149 @@ class SwaptionTest(unittest.TestCase):
     def _assert_swaption_delta(self,
                                swaption_pricer_func,
                                use_bachelier_vol: bool):
-        bump = 1.e-4
-        epsilon = 1.e-10
-
         strikes = [0.03, 0.04, 0.05, 0.06, 0.07]
         vols = [0.01, 0.10, 0.20, 0.30, 0.70, 0.90]
 
-        types = [ql.Settlement.Physical, ql.Settlement.Cash]
-        methods = [ql.Settlement.PhysicalOTC,
-                   ql.Settlement.CollateralizedCashPrice]
+        settle_map = {ql.Settlement.PhysicalOTC: ql.Settlement.Physical,
+                      ql.Settlement.CollateralizedCashPrice: ql.Settlement.Cash,
+                      ql.Settlement.ParYieldCurve: ql.Settlement.Cash}
 
         for v in vols:
             for e in self.exercises:
                 for l in self.lengths:
                     for s in strikes:
-                        for h in range(2):
-                            volatility = v / 100.0 if use_bachelier_vol else v
-                            swaption_engine = swaption_pricer_func(
-                                self.discount_handle, volatility)
-                            exercise_date = self.calendar.advance(
-                                self.today, e)
-                            start_date = self.calendar.advance(
-                                exercise_date, ql.Period(2, ql.Days))
+                        for t in self.swap_type:
+                            for s_m, s_t in settle_map.items():
+                                volatility = v / 100.0 if use_bachelier_vol else v
+                                swaption_engine = swaption_pricer_func(
+                                    self.discount_handle, volatility)
+                                exercise_date = self.calendar.advance(
+                                    self.today, e)
+                                start_date = self.calendar.advance(
+                                    exercise_date, ql.Period(2, ql.Days))
 
-                            self.projection_quote_handle.linkTo(
-                                ql.SimpleQuote(self.projection_rate))
+                                self.projection_quote_handle.linkTo(
+                                    ql.SimpleQuote(self.projection_rate))
 
-                            underlying = ql.MakeVanillaSwap(
-                                l, self.idx, s, ql.Period(0, ql.Days),
-                                effectiveDate=start_date,
-                                fixedLegTenor=ql.Period(1, ql.Years),
-                                fixedLegDayCount=ql.Thirty360(),
-                                floatingLegSpread=0.0,
-                                swapType=self.swap_type[h])
-                            underlying.setPricingEngine(self.swap_engine)
+                                underlying = ql.MakeVanillaSwap(
+                                    l, self.idx, s, ql.Period(0, ql.Days),
+                                    effectiveDate=start_date,
+                                    fixedLegTenor=ql.Period(1, ql.Years),
+                                    fixedLegDayCount=ql.Thirty360(),
+                                    floatingLegSpread=0.0,
+                                    swapType=t)
+                                underlying.setPricingEngine(self.swap_engine)
 
-                            fair_rate = underlying.fairRate()
+                                fair_rate = underlying.fairRate()
 
-                            swaption = ql.Swaption(underlying,
-                                                   ql.EuropeanExercise(
-                                                       exercise_date),
-                                                   types[h],
-                                                   methods[h])
-                            swaption.setPricingEngine(swaption_engine)
+                                swaption = ql.Swaption(underlying,
+                                                       ql.EuropeanExercise(
+                                                           exercise_date),
+                                                       s_t,
+                                                       s_m)
+                                swaption.setPricingEngine(swaption_engine)
 
-                            value = swaption.NPV()
-                            delta = swaption.delta() * bump
+                                value = swaption.NPV()
+                                delta = swaption.delta() * BASIS_POINT
 
-                            self.projection_quote_handle.linkTo(
-                                ql.SimpleQuote(self.projection_rate + bump))
+                                self.projection_quote_handle.linkTo(
+                                    ql.SimpleQuote(self.projection_rate + BASIS_POINT))
 
-                            bumped_fair_rate = underlying.fairRate()
-                            bumped_value = swaption.NPV()
-                            bumped_delta = swaption.delta() * bump
+                                bumped_fair_rate = underlying.fairRate()
+                                bumped_value = swaption.NPV()
+                                bumped_delta = swaption.delta() * BASIS_POINT
 
-                            delta_bump = bumped_fair_rate - fair_rate
-                            approx_delta = (bumped_value - value) / \
-                                delta_bump * bump
+                                delta_bump = bumped_fair_rate - fair_rate
+                                approx_delta = (bumped_value - value) / \
+                                    delta_bump * BASIS_POINT
 
-                            lower_bound = min(delta, bumped_delta) - epsilon
-                            upper_bound = max(delta, bumped_delta) + epsilon
+                                lower_bound = min(
+                                    delta, bumped_delta) - EPSILON
+                                upper_bound = max(
+                                    delta, bumped_delta) + EPSILON
 
-                            # Based on the Mean Value Theorem, the below inequality
-                            # should hold for any function that is monotonic in the
-                            # area of the bump.
-                            check_is_correct = (lower_bound <= approx_delta) and (
-                                approx_delta <= upper_bound)
+                                # Based on the Mean Value Theorem, the below inequality
+                                # should hold for any function that is monotonic in the
+                                # area of the bump.
+                                check_is_correct = (lower_bound <= approx_delta) and (
+                                    approx_delta <= upper_bound)
 
-                            fail_msg = f""" Swaption delta test failed for:
-                                                option tenor: {e}
-                                                volatility : {volatility}
-                                                option type: {OPTION_TYPE_MAP[swaption.type()]}
-                                                swap tenor: {l}
-                                                strike: {s}
-                                                settlement: {SETTLEMENT_TYPE_MAP[swaption.settlementType()]}
-                                                method: {SETTLEMENT_METHOD_MAP[swaption.settlementMethod()]}
-                                                delta: {delta}
-                                                approx delta: {approx_delta}
-                                        """
-                            self.assertTrue(check_is_correct, msg=fail_msg)
+                                fail_msg = f""" Swaption delta test failed for:
+                                                    option tenor: {e}
+                                                    volatility : {volatility}
+                                                    option type: {OPTION_TYPE_MAP[t]}
+                                                    swap tenor: {l}
+                                                    strike: {s}
+                                                    settlement: {SETTLEMENT_TYPE_MAP[s_t]}
+                                                    method: {SETTLEMENT_METHOD_MAP[s_m]}
+                                                    delta: {delta}
+                                                    approx delta: {approx_delta}
+                                            """
+                                self.assertTrue(check_is_correct, msg=fail_msg)
 
     def _assert_swaption_annuity(self,
                                  swaption_pricer_func,
                                  use_bachelier_vol: bool):
-        basis_point = 1.e-4
-        epsilon = 1.e-10
-        day_count = ql.Thirty360()
-
         self.projection_quote_handle.linkTo(
             ql.SimpleQuote(self.projection_rate))
 
-        settlement_type = ql.Settlement.Cash
+        settle_type = ql.Settlement.Cash
         methods = [ql.Settlement.ParYieldCurve,
                    ql.Settlement.CollateralizedCashPrice]
 
         for e in self.exercises:
             for l in self.lengths:
-                for h in range(2):
-                    volatility = 0.003 if use_bachelier_vol else 0.3
-                    strike = 0.03
-                    swaption_engine = swaption_pricer_func(
-                        self.discount_handle, volatility)
-                    exercise_date = self.calendar.advance(
-                        self.today, e)
-                    start_date = self.calendar.advance(
-                        exercise_date, ql.Period(2, ql.Days))
+                for t in self.swap_type:
+                    for m in methods:
+                        volatility = 0.003 if use_bachelier_vol else 0.3
+                        strike = 0.03
+                        swaption_engine = swaption_pricer_func(
+                            self.discount_handle, volatility)
+                        exercise_date = self.calendar.advance(
+                            self.today, e)
+                        start_date = self.calendar.advance(
+                            exercise_date, ql.Period(2, ql.Days))
 
-                    underlying = ql.MakeVanillaSwap(
-                        l, self.idx, strike, ql.Period(0, ql.Days),
-                        effectiveDate=start_date,
-                        fixedLegTenor=ql.Period(1, ql.Years),
-                        fixedLegDayCount=day_count,
-                        floatingLegSpread=0.0,
-                        swapType=self.swap_type[h])
-                    underlying.setPricingEngine(self.swap_engine)
+                        underlying = ql.MakeVanillaSwap(
+                            l, self.idx, strike, ql.Period(0, ql.Days),
+                            effectiveDate=start_date,
+                            fixedLegTenor=ql.Period(1, ql.Years),
+                            fixedLegDayCount=ql.Thirty360(),
+                            floatingLegSpread=0.0,
+                            swapType=t)
+                        underlying.setPricingEngine(self.swap_engine)
 
-                    swaption = ql.Swaption(underlying,
-                                           ql.EuropeanExercise(exercise_date),
-                                           settlement_type,
-                                           methods[h])
-                    swaption.setPricingEngine(swaption_engine)
+                        swaption = ql.Swaption(underlying,
+                                               ql.EuropeanExercise(
+                                                   exercise_date),
+                                               settle_type,
+                                               m)
+                        swaption.setPricingEngine(swaption_engine)
 
-                    fair_rate = underlying.fairRate()
-                    annuity = swaption.annuity()
+                        annuity = swaption.annuity()
+                        expected_annuity = 0.0
+                        if (m == ql.Settlement.CollateralizedCashPrice):
+                            expected_annuity = swap_pv01(underlying)
+                        if (m == ql.Settlement.ParYieldCurve):
+                            expected_annuity = par_yield_bps(
+                                underlying, self.discount_handle)
 
-                    expected_annuity = 0.0
-                    if (methods[h] == ql.Settlement.CollateralizedCashPrice):
-                        expected_annuity = abs(
-                            underlying.fixedLegBPS()) / basis_point
-                    if (methods[h] == ql.Settlement.ParYieldCurve):
-                        fixed_leg = underlying.fixedLeg()
-                        first_coupon = ql.as_fixed_rate_coupon(fixed_leg[0])
-                        discount_date = first_coupon.accrualStartDate()
-                        fixed_leg_cash_bps = ql.CashFlows.bps(
-                            fixed_leg,
-                            ql.InterestRate(
-                                fair_rate, day_count, ql.Compounded, ql.Annual),
-                            False,
-                            discount_date)
-                        expected_annuity = abs(
-                            fixed_leg_cash_bps / basis_point) * (
-                                self.discount_handle.discount(discount_date))
-
-                    fail_msg = f""" Swaption annuity test failed for:
-                                        option tenor: {e}
-                                        volatility : {volatility}
-                                        option type: {OPTION_TYPE_MAP[swaption.type()]}
-                                        swap tenor: {l}
-                                        strike: {strike}
-                                        settlement: {SETTLEMENT_TYPE_MAP[swaption.settlementType()]}
-                                        method: {SETTLEMENT_METHOD_MAP[swaption.settlementMethod()]}
-                                        annuity: {annuity}
-                                        replicated annuity: {expected_annuity}
-                                    """
-                    self.assertAlmostEquals(
-                        first=annuity,
-                        second=expected_annuity,
-                        delta=epsilon,
-                        msg=fail_msg)
+                        fail_msg = f""" Swaption annuity test failed for:
+                                            option tenor: {e}
+                                            volatility : {volatility}
+                                            option type: {OPTION_TYPE_MAP[t]}
+                                            swap tenor: {l}
+                                            strike: {strike}
+                                            settlement: {SETTLEMENT_TYPE_MAP[settle_type]}
+                                            method: {SETTLEMENT_METHOD_MAP[m]}
+                                            annuity: {annuity}
+                                            replicated annuity: {expected_annuity}
+                                        """
+                        self.assertAlmostEquals(
+                            first=annuity,
+                            second=expected_annuity,
+                            delta=EPSILON,
+                            msg=fail_msg)
 
     def test_swaption_delta_black_volatility(self):
         """Testing swaption delta in Black model"""
