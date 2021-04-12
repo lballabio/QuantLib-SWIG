66805decdd12e6a6b5994a9212ac7ba5ddfd49c0
Luigi Ballabio <luigi.ballabio@gmail.com>
2021-04-12 11:12:19 +0200

    Set version to 1.29 final.

--- Python/setup.py
+++ Python/setup.py
@@ -212,7 +212,7 @@ classifiers = [
 ]
 
 setup(name             = "QuantLib",
-      version          = "1.29-rc",
+      version          = "1.29",
       description      = "Python bindings for the QuantLib library",
       long_description = """
 QuantLib (https://www.quantlib.org/) is a C++ library for financial quantitative
