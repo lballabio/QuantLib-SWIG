cb284189a2257ab6b5299f5a0dbd2180fee7b833
Luigi Ballabio <luigi.ballabio@gmail.com>
2023-10-20 11:34:41 +0200

    Set version to 1.35-dev

--- Python/setup.py
+++ Python/setup.py
@@ -224,7 +224,7 @@ classifiers = [
 ]
 
 setup(name             = "QuantLib",
-      version          = "1.34",
+      version          = "1.35-dev",
       description      = "Python bindings for the QuantLib library",
       long_description = """
 QuantLib (https://www.quantlib.org/) is a free/open-source C++ library
