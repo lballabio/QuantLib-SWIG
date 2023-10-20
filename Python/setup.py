f538e0e8a6ffd8e1819ed9d4f1121d6a74db7b59
Luigi Ballabio <luigi.ballabio@gmail.com>
2023-10-20 11:34:41 +0200

    Set version to 1.40-dev

--- Python/setup.py
+++ Python/setup.py
@@ -207,7 +207,7 @@ with open("./setup.cfg", "w") as f:
 
 setup(
     name="QuantLib",
-    version="1.39",
+    version="1.40-dev",
     description="Python bindings for the QuantLib library",
     long_description=long_description,
     long_description_content_type="text/x-rst",
