68b24e7b03b0ca1f8747abd95efb22bf75026c00
Luigi Ballabio <luigi.ballabio@gmail.com>
2024-10-07 14:29:46 +0200

    Set version to 1.38-rc

--- Python/setup.py
+++ Python/setup.py
@@ -202,7 +202,7 @@ py_limited_api = platform.python_implementation() == "CPython"
 
 setup(
     name="QuantLib",
-    version="1.38-dev",
+    version="1.38-rc",
     description="Python bindings for the QuantLib library",
     long_description=long_description,
     long_description_content_type="text/x-rst",
